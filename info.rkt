9eee74ae6ce45a1ede3d5f0db90ce74d850e0ace
sonnynajar <sonny.najar@gmail.com>
2020-02-20 10:39:14 -0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "ts-icons")
 (define deps '("base" "define-assets-from"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "common-icons"))
 (define scribblings '(("scribblings/common-icons.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
