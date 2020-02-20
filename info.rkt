aed730fa157e3bb8c8a0ee5b639ed15a6097e49c
sonnynajar <sonny.najar@gmail.com>
2020-02-20 10:47:27 -0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "ts-icons")
 (define deps '("base" "define-assets-from"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/common-icons.scrbl" ())))
+(define scribblings '(("scribblings/ts-icons.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(thoughtstem))
