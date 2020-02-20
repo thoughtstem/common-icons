2b884e504341973ad789f10f804f0dc9a71a708b
sonnynajar <sonny.najar@gmail.com>
2020-02-20 11:16:29 -0800

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "ts-icons")
-(define deps '("base" "define-assets-from"))
+(define deps '("base" 
+               "https://github.com/thoughtstem/define-assets-from.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/ts-icons.scrbl" ())))
 (define pkg-desc "Description Here")
